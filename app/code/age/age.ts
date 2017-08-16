2b8d77d03ca80a70722ce04a1603065615232f05
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-08-16 18:26:49 -0300

    update

--- app/code/age/age.ts
+++ app/code/age/age.ts
@@ -4,6 +4,13 @@ class Age extends Component implements AppObject {
     }
 
     public result(){
-        return "25";
+        var today = new Date();
+        var birthDate = new Date("07/01/1992");
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
     }
 }
