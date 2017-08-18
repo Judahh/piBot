e7dca0c4052b1eedc4566478bb7aa5a78168d313
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-08-18 11:42:29 -0300

    update

---
+++ clone.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo git clone https://github.com/Judahh/backApp.git backApp &
+https://github.com/Judahh/backApi.git backApi
+ret2=$?   # get value returned by b.sh
+wait %1   # Wait for a.sh to finish
+ret1=$?   # get value returned by a.sh
+echo "$ret1: $ret2: $ret3: $ret4: $ret5"
